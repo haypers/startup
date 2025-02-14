0859cab26ae47b2b743dec06950e1c9b107245ad
Hayden Perkes <haypers@gmail.com>
2025-02-13 21:52:01 -0700

    got nav working

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Signin } from './signin/signin';
+import { Pixels } from './pixels/pixels';
+import { History } from './history/history';
+import { About } from './about/about';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
-}
+    return (
+      <BrowserRouter>
+        <div className="body bg-dark text-light">
+            <header>
+            <h1>The Public Pixel</h1>
+            <nav>
+                <ul>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="">
+                            The Pixels
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="signin">
+                            Sign In
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="about">
+                            About
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="history">
+                            Art History
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            </header>
+
+            <Routes>
+                <Route path='/' element={<Pixels />} exact />
+                <Route path='/signin' element={<Signin />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/history' element={<History />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+
+            <footer>
+            <span className="text-reset">Author Name:</span>
+            <a href="https://github.com/haypers/startup">Hayden Perkes</a>
+            </footer>
+        </div>
+      </BrowserRouter>
+    );
+  }
+
+
+  function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
---
+++ src/pixels/pixels.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Pixels() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>pixels displayed here</div>
+    </main>
+  );
+}
